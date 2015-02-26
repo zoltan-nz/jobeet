f65a6495a9e06d06639cd450459046dae8ae2791
Zoltan <zdebre@gmail.com>
2015-02-26 23:34:30 +1300

    Create database and fixture data

--- lib/form/doctrine/base/BaseJobeetAffiliateForm.class.php
+++ lib/form/doctrine/base/BaseJobeetAffiliateForm.class.php
@@ -15,24 +15,30 @@ abstract class BaseJobeetAffiliateForm extends BaseFormDoctrine
   public function setup()
   {
     $this->setWidgets(array(
-      'id'         => new sfWidgetFormInputHidden(),
-      'url'        => new sfWidgetFormInputText(),
-      'email'      => new sfWidgetFormInputText(),
-      'token'      => new sfWidgetFormInputText(),
-      'is_active'  => new sfWidgetFormInputText(),
-      'created_at' => new sfWidgetFormDateTime(),
-      'updated_at' => new sfWidgetFormDateTime(),
+      'id'                     => new sfWidgetFormInputHidden(),
+      'url'                    => new sfWidgetFormInputText(),
+      'email'                  => new sfWidgetFormInputText(),
+      'token'                  => new sfWidgetFormInputText(),
+      'is_active'              => new sfWidgetFormInputCheckbox(),
+      'created_at'             => new sfWidgetFormDateTime(),
+      'updated_at'             => new sfWidgetFormDateTime(),
+      'jobeet_categories_list' => new sfWidgetFormDoctrineChoice(array('multiple' => true, 'model' => 'JobeetCategory')),
     ));
 
     $this->setValidators(array(
-      'id'         => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
-      'url'        => new sfValidatorString(array('max_length' => 255)),
-      'email'      => new sfValidatorString(array('max_length' => 255)),
-      'token'      => new sfValidatorString(array('max_length' => 255)),
-      'is_active'  => new sfValidatorInteger(array('required' => false)),
-      'created_at' => new sfValidatorDateTime(),
-      'updated_at' => new sfValidatorDateTime(),
+      'id'                     => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
+      'url'                    => new sfValidatorString(array('max_length' => 255)),
+      'email'                  => new sfValidatorString(array('max_length' => 255)),
+      'token'                  => new sfValidatorString(array('max_length' => 255)),
+      'is_active'              => new sfValidatorBoolean(array('required' => false)),
+      'created_at'             => new sfValidatorDateTime(),
+      'updated_at'             => new sfValidatorDateTime(),
+      'jobeet_categories_list' => new sfValidatorDoctrineChoice(array('multiple' => true, 'model' => 'JobeetCategory', 'required' => false)),
     ));
+
+    $this->validatorSchema->setPostValidator(
+      new sfValidatorDoctrineUnique(array('model' => 'JobeetAffiliate', 'column' => array('email')))
+    );
 
     $this->widgetSchema->setNameFormat('jobeet_affiliate[%s]');
 
@@ -46,6 +52,58 @@ abstract class BaseJobeetAffiliateForm extends BaseFormDoctrine
   public function getModelName()
   {
     return 'JobeetAffiliate';
+  }
+
+  public function updateDefaultsFromObject()
+  {
+    parent::updateDefaultsFromObject();
+
+    if (isset($this->widgetSchema['jobeet_categories_list']))
+    {
+      $this->setDefault('jobeet_categories_list', $this->object->JobeetCategories->getPrimaryKeys());
+    }
+
+  }
+
+  protected function doUpdateObject($values)
+  {
+    $this->updateJobeetCategoriesList($values);
+
+    parent::doUpdateObject($values);
+  }
+
+  public function updateJobeetCategoriesList($values)
+  {
+    if (!isset($this->widgetSchema['jobeet_categories_list']))
+    {
+      // somebody has unset this widget
+      return;
+    }
+
+    if (!array_key_exists('jobeet_categories_list', $values))
+    {
+      // no values for this widget
+      return;
+    }
+
+    $existing = $this->object->JobeetCategories->getPrimaryKeys();
+    $values = $values['jobeet_categories_list'];
+    if (!is_array($values))
+    {
+      $values = array();
+    }
+
+    $unlink = array_diff($existing, $values);
+    if (count($unlink))
+    {
+      $this->object->unlink('JobeetCategories', array_values($unlink));
+    }
+
+    $link = array_diff($values, $existing);
+    if (count($link))
+    {
+      $this->object->link('JobeetCategories', array_values($link));
+    }
   }
 
 }
--- lib/form/doctrine/base/BaseJobeetCategoryForm.class.php
+++ lib/form/doctrine/base/BaseJobeetCategoryForm.class.php
@@ -15,18 +15,24 @@ abstract class BaseJobeetCategoryForm extends BaseFormDoctrine
   public function setup()
   {
     $this->setWidgets(array(
-      'id'         => new sfWidgetFormInputHidden(),
-      'name'       => new sfWidgetFormInputText(),
-      'created_at' => new sfWidgetFormDateTime(),
-      'updated_at' => new sfWidgetFormDateTime(),
+      'id'                     => new sfWidgetFormInputHidden(),
+      'name'                   => new sfWidgetFormInputText(),
+      'created_at'             => new sfWidgetFormDateTime(),
+      'updated_at'             => new sfWidgetFormDateTime(),
+      'jobeet_affiliates_list' => new sfWidgetFormDoctrineChoice(array('multiple' => true, 'model' => 'JobeetAffiliate')),
     ));
 
     $this->setValidators(array(
-      'id'         => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
-      'name'       => new sfValidatorString(array('max_length' => 255)),
-      'created_at' => new sfValidatorDateTime(),
-      'updated_at' => new sfValidatorDateTime(),
+      'id'                     => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
+      'name'                   => new sfValidatorString(array('max_length' => 255)),
+      'created_at'             => new sfValidatorDateTime(),
+      'updated_at'             => new sfValidatorDateTime(),
+      'jobeet_affiliates_list' => new sfValidatorDoctrineChoice(array('multiple' => true, 'model' => 'JobeetAffiliate', 'required' => false)),
     ));
+
+    $this->validatorSchema->setPostValidator(
+      new sfValidatorDoctrineUnique(array('model' => 'JobeetCategory', 'column' => array('name')))
+    );
 
     $this->widgetSchema->setNameFormat('jobeet_category[%s]');
 
@@ -40,6 +46,58 @@ abstract class BaseJobeetCategoryForm extends BaseFormDoctrine
   public function getModelName()
   {
     return 'JobeetCategory';
+  }
+
+  public function updateDefaultsFromObject()
+  {
+    parent::updateDefaultsFromObject();
+
+    if (isset($this->widgetSchema['jobeet_affiliates_list']))
+    {
+      $this->setDefault('jobeet_affiliates_list', $this->object->JobeetAffiliates->getPrimaryKeys());
+    }
+
+  }
+
+  protected function doUpdateObject($values)
+  {
+    $this->updateJobeetAffiliatesList($values);
+
+    parent::doUpdateObject($values);
+  }
+
+  public function updateJobeetAffiliatesList($values)
+  {
+    if (!isset($this->widgetSchema['jobeet_affiliates_list']))
+    {
+      // somebody has unset this widget
+      return;
+    }
+
+    if (!array_key_exists('jobeet_affiliates_list', $values))
+    {
+      // no values for this widget
+      return;
+    }
+
+    $existing = $this->object->JobeetAffiliates->getPrimaryKeys();
+    $values = $values['jobeet_affiliates_list'];
+    if (!is_array($values))
+    {
+      $values = array();
+    }
+
+    $unlink = array_diff($existing, $values);
+    if (count($unlink))
+    {
+      $this->object->unlink('JobeetAffiliates', array_values($unlink));
+    }
+
+    $link = array_diff($values, $existing);
+    if (count($link))
+    {
+      $this->object->link('JobeetAffiliates', array_values($link));
+    }
   }
 
 }
