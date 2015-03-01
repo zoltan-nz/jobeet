6581b301ed7c21a5e0072a724f233f3108d08d74
Zoltan <zdebre@gmail.com>
2015-03-01 15:11:47 +1300

    List is group by categories on home page

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -12,15 +12,15 @@ class jobActions extends sfActions
 {
   public function executeIndex(sfWebRequest $request)
   {
-    $this->jobeet_jobs = Doctrine_Core::getTable('JobeetJob')
-      ->createQuery('a')
-      ->execute();
+    $this->categories = JobeetCategoryTable::getInstance()->getWithJobs();
   }
 
+  /**
+   * @param sfWebRequest $request
+   */
   public function executeShow(sfWebRequest $request)
   {
     $this->job = $this->getRoute()->getObject();
-    $this->forward404Unless($this->job);
   }
 
   public function executeNew(sfWebRequest $request)
--- apps/frontend/modules/job/templates/indexSuccess.php
+++ apps/frontend/modules/job/templates/indexSuccess.php
@@ -1,28 +1,35 @@
-<?php
-/**
- * @var $jobeet_jobs JobeetJob
- */
-?>
+<?php foreach ($categories as $category): ?>
+
+  <div class="category_<?= Jobeet::slugify($category->getName()) ?>">
+    <div class="category">
+      <div class="feed">
+        <a href="">Feed</a>
+      </div>
+      <h1><?= $category ?></h1>
+    </div>
 
 
+  </div>
 
-<table class="table">
-  <thead>
-    <tr>
-      <th>Location</th>
-      <th>Position</th>
-      <th>Company</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($jobeet_jobs as $job): ?>
-    <tr>
-      <td><?= $job->getLocation() ?></td>
-      <td><?= link_to($job->getPosition(), 'job_show_user', $job) ?></td>
-      <td><?= $job->getCompany() ?></td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Location</th>
+        <th>Position</th>
+        <th>Company</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($category->getActiveJobs(sfConfig::get('app_max_jobs_on_homepage')) as $job): ?>
+      <tr>
+        <td><?= $job->getLocation() ?></td>
+        <td><?= link_to($job->getPosition(), 'job_show_user', $job) ?></td>
+        <td><?= $job->getCompany() ?></td>
+      </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
 
-  <a href="<?php echo url_for('job/new') ?>">New</a>
+<?php endforeach ?>
+
+<a href="<?php echo url_for('job/new') ?>">New</a>
