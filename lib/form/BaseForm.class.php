9f65baf67e6fbc8eb274332c1d113947b40ea036
Zoltan <zdebre@gmail.com>
2015-03-16 01:08:49 +1300

    Add email validation

--- lib/form/BaseForm.class.php
+++ lib/form/BaseForm.class.php
@@ -12,5 +12,9 @@ class BaseForm extends sfFormSymfony
 {
   public function configure() {
     $this->useFields(array('category_id', 'type', 'company', 'logo', 'url', 'position', 'location', 'description', 'how_to_apply', 'token', 'is_public', 'email'));
+    $this->validatorSchema['email'] = new sfValidatorAnd(array(
+      $this->validatorSchema['email'],
+      new sfValidatorEmail(),
+    ));
   }
 }
